3d51370750fc65973c2d173a5cf630ac57fe35e9
pwsscode <pedrosimplicio.code@gmail.com>
2024-02-19 17:36:54 -0300

    1736

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.addEventListener('scroll', function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 650)
+    header.classList.toggle('rolagem', window.scrollY > 500)
 })
+
+function mostrarConteudo(id) {
+    const paragrafos = document.querySelectorAll('p');
+    const imagens = document.querySelectorAll('img, #1');
+
+    // Esconde todos os parágrafos e imagens
+    paragrafos.forEach(p => p.style.display = 'none');
+    imagens.forEach(img => img.style.display = 'none');
+
+    // Exibe o parágrafo e imagem correspondentes
+    document.getElementById(id).style.display = 'block';
+    document.getElementById('imagem' + id.slice(9)).style.display = 'block';
+}
+
+// Exibe apenas o primeiro parágrafo e a primeira imagem inicialmente
+mostrarConteudo('paragrafo1');
